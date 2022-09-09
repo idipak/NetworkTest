ca623a3e7febe5f679193419f1af2ab8444804c8
Dipak <dipak_pd@outlook.com>
2022-09-09 16:55:19 +0530

    Initial Commit

---
+++ NetworkTest/NetworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTestApp.swift
+//  NetworkTest
+//
+//  Created by Codebucket on 09/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
